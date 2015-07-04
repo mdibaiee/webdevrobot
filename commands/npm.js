38aaaf4343ed841b920f444051734c266952995a
Ali Movahedi <aligeekjs@gmail.com>
2015-07-04 19:52:12 +0430

    remove web scraping

--- commands/npm.js
+++ commands/npm.js
@@ -1,24 +1,26 @@
 import Message from 'telegram-api/types/Message';
 import request from 'request';
-import cheerio from 'cheerio';
 
 export default function npm(bot) {
 
-  const BASE = 'https://www.npmjs.com';
-  const METHOD = '/search?q=';
+  const BASE = 'http://registry.npmjs.org/';
 
   const ask = new Message().text('What package are you searching for?');
   bot.command('npm', message => {
 
     const search = (pack, id) => {
       new Promise((resolve, reject) => {
-        request(BASE + METHOD + pack, (err, res, body) => {
-          const $ = cheerio.load(body);
-          const result = $('.search-results > li:nth-child(1) > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').attr('href');
-          result ? resolve([pack, result]) : reject();
+        request(BASE + pack, (err, res, body) => {
+          try {
+            const result = JSON.parse(body);
+            resolve([pack, result.name])
+          }
+          catch(e) {
+            reject();
+          }
         });
       }).then((result) => {
-        bot.send(new Message().text(result[0] + '\n' + BASE + result[1]).to(id))
+        bot.send(new Message().text(result[0] + '\n' + 'https://www.npmjs.com/' + result[1]).to(id))
       }).catch(() => {
         bot.send(new Message().text('No results found :(').to(id));
       });
