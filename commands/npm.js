bc9975329602a71a52d5e34970eb2839e62f3acc
amovah <aligeekjs@gmail.com>
2015-07-08 13:13:59 +0430

    Fix /npm message

--- commands/npm.js
+++ commands/npm.js
@@ -5,14 +5,15 @@ import bot from '../bot'
 bot.command('npm <pkg> [+count]', message => {
   if (!message.args.pkg) return;
   search(message.args.pkg, message.args.count).then(items => {
+    if (!items.length) {
+      const msg = new Message().text('No results found :(').to(message.chat.id);
+
+      bot.send(msg);
+    }
     for (let {name, url} of items) {
       const msg = new Message().text(`${name}\n${url}`).to(message.chat.id);
       bot.send(msg);
     }
-  }, () => {
-    const msg = new Message().text('No results found :(').to(message.chat.id);
-
-    bot.send(msg);
   });
 });
 
