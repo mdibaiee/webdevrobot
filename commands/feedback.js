932ba02d88576a21e8365541ed469f6b14670447
Mahdi Dibaiee <mdibaiee@aol.com>
2015-07-13 05:40:03 +0430

    Answer feedbacks with a thanks message

--- commands/feedback.js
+++ commands/feedback.js
@@ -2,12 +2,16 @@ import bot from '../bot';
 import Message from 'telegram-api/types/Message';
 
 const DEV_ID = 33352579;
+const THANKS = 'Thanks for helping us improve our bot!';
+
 bot.command('feedback ...text', message => {
   const username = message.from.username;
   const text = message.args.text;
   const msg = new Message().to(DEV_ID)
                            .text(`Feedback from @${username}\n${text}`);
   bot.send(msg);
+
+  bot.send(new Message().text(THANKS).to(message.chat.id));
 });
 
 export default {
