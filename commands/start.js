c774a0e123fc4c5e1ab3f7a4f04787df529fb542
Mahdi Dibaiee <mdibaiee@aol.com>
2015-07-13 09:13:44 +0430

    Rate note

--- commands/start.js
+++ commands/start.js
@@ -4,6 +4,8 @@ import Message from 'telegram-api/types/Message';
 const start = new Message().text(`Hello!
 I give you services to search across various sources such as MDN, GitHub, etc.
 
+If you like this bot, please rate it: @storebot => /rate
+
 Try /help`);
 bot.command('start', message => {
   bot.send(start.to(message.chat.id));
